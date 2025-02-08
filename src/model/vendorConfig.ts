9982ecadd8d3141a6d292be4a9643f3934a903ff
AlexaZhou <AlexaZhou@163.com>
2025-02-08 22:03:20 +0800

    add vendor model

---
+++ src/model/vendorConfig.ts
@@ -0,0 +1,21 @@
+import { Model } from 'sutando';
+import { v4 as uuid } from 'uuid';
+
+
+class VendorConfig extends Model {
+    table = 'vendor';
+
+    id!: number;
+    type!: string;
+    name!: string;
+    token!: string;
+    url!: string;
+
+    created_at!: Date;
+    updated_at!: Date;
+}
+
+
+export {
+    VendorConfig
+}
